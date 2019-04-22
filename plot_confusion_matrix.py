0924d5e2e2a0118a3288d6747ae1e4812dc04cfc
Herman Stavelin <staveliney@gmail.com>
2019-04-22 14:13:00 +0200

    refactored plotting of confusion_matrix

--- plot_confusion_matrix.py
+++ plot_confusion_matrix.py
@@ -1,28 +1,7 @@
-print(__doc__)
-
 import numpy as np
 import matplotlib.pyplot as plt
 
-from sklearn import svm, datasets
-from sklearn.model_selection import train_test_split
 from sklearn.metrics import confusion_matrix
-from sklearn.utils.multiclass import unique_labels
-
-# import some data to play with
-iris = datasets.load_iris()
-X = iris.data
-y = iris.target
-class_names = iris.target_names
-
-# Split the data into a training set and a test set
-X_train, X_test, y_train, y_test = train_test_split(X, y, random_state=0)
-
-# Run classifier, using a model that is too regularized (C too low) to see
-# the impact on the results
-classifier = svm.SVC(kernel='linear', C=0.01)
-y_pred = classifier.fit(X_train, y_train).predict(X_test)
-
-
 def plot_confusion_matrix(y_true, y_pred, classes,
                           normalize=False,
                           title=None,
@@ -75,19 +54,6 @@ def plot_confusion_matrix(y_true, y_pred, classes,
                     color="white" if cm[i, j] > thresh else "black")
     fig.tight_layout()
     return ax
-
-
-#np.set_printoptions(precision=2)
-
-# Plot non-normalized confusion matrix
-#plot_confusion_matrix(y_test, y_pred, classes=class_names,
-                      #title='Confusion matrix, without normalization')
-
 # Plot normalized confusion matrix
 #plot_confusion_matrix(y_test, y_pred, classes=class_names, normalize=True,
                       #title='Normalized confusion matrix')
-
-#plt.show()
-
-if __name__=="__main__":
-    main()
