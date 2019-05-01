9a710c8203588cc826260a1371af5e3b0d01245f
ersol <erling.solberg@hotmail.com>
2019-05-01 12:15:37 +0200

    Made proper setup script

--- Wovels/Wovels/datadecrypt.m
+++ Wovels/Wovels/datadecrypt.m
@@ -3,7 +3,9 @@
 % characters 4-5:  vowel (ae=”had”, ah=”hod”, aw=”hawed”, eh=”head”,
 %    er=”heard”, ei=”haid”, ih=”hid”, iy=”heed”, oa=/o/ as in “boat”, 
 %    oo=”hood”, uh=”hud”, uw=”who’d”) 
- 
+
+[files,dur,F0s,F1s,F2s,F3s,F4s,F120,F220,F320,F150,F250,F350,F180,F280,F380] = textread('vowdata_nohead.dat','%s%4.1f%4.1f%4.1f%4.1f%4.1f%4.1f%4.1f%4.1f%4.1f%4.1f%4.1f%4.1f%4.1f%4.1f%4.1f'); 
+
 vowel = str2mat('ae','ah','aw','eh','er','ei','ih','iy','oa','oo','uh','uw');
 talker_group = str2mat('m','w','b','g');
 filenames=char(files); % convert cell array to character matrix
